72ef1ae3ff325a1d7fd8da562817fc586fd65e60
Brandon Hammond <newdaynewburner@gmail.com>
2024-11-17 15:10:51 -0600

    Added multithreading support for youtube-to-mp3/album_downloader.py

--- youtube-to-mp3/album_downloader.py
+++ youtube-to-mp3/album_downloader.py
@@ -9,6 +9,7 @@ Download MP3 format albums from YouTube playlists
 import os
 import sys
 import subprocess
+import threading
 import eyed3
 from pytubefix import YouTube, Playlist
 
@@ -17,7 +18,41 @@ from pytubefix import YouTube, Playlist
 # scripts settings and change how it works :)
 config = {
     "automatically_create_album_dir": True,
+    "automatically_generate_song_title": False,
+    "enable_multithreading": True
 }
+
+def download_thread(video_url, dl_filename, album_id3_tags, song_id3_tags):
+    """ Download, convert, and tag individual songs
+    
+    Arguments:
+        video_url - string - YouTube video URL
+        dl_filename - filename - Filename of song download
+        album_id3_tags - dict - Contains album ID3 medadata info
+        song_id3_tags - dict - Contains song ID3 metadata info
+        
+    Returns:
+        None
+    """
+    
+    yt = YouTube(video_url)
+    
+    raw_audio = yt.streams.filter(only_audio=True).first()
+    raw_audio_file = raw_audio.download()
+
+    convert_command = """ffmpeg -hide_banner -loglevel error -i "{0}" "{1}" """.format(raw_audio_file, dl_filename)
+    subprocess.check_output(convert_command, shell=True)
+    os.remove(raw_audio_file)
+
+    mp3_file = eyed3.load(dl_filename)
+    mp3_file.initTag()
+    mp3_file.tag.track_num = song_id3_tags["track_num"]
+    mp3_file.tag.title = song_id3_tags["title"]
+    mp3_file.tag.artist = album_id3_tags["artist"]
+    mp3_file.tag.album = album_id3_tags["album_name"]
+    mp3_file.tag.genre = album_id3_tags["genre"]
+    mp3_file.tag.year = album_id3_tags["year"]
+    mp3_file.tag.save()
 
 def album_downloader(config, argv):
     """ Download all videos in a playlist, convert to MP3, and 
@@ -56,6 +91,7 @@ def album_downloader(config, argv):
         print("")
         
         video_urls = pl.video_urls
+        dl_threads = []
         track_num = 1
         for video_url in video_urls:
             yt = YouTube(video_url)
@@ -64,34 +100,26 @@ def album_downloader(config, argv):
             print("")
             print("Please provide song-specific tag info:")
             song_id3_tags = {}
-            
             song_id3_tags["track_num"] = track_num
             song_id3_tags["title"] = input("[Song title]: ")
+            print("")
             
             dl_filename = "{0}. {1}.mp3".format(song_id3_tags["track_num"], song_id3_tags["title"])
             
-            print("Downloading...")
-            raw_audio = yt.streams.filter(only_audio=True).first()
-            raw_audio_file = raw_audio.download()
-            
-            print("Converting...")
-            convert_command = """ffmpeg -hide_banner -loglevel error -i "{0}" "{1}" """.format(raw_audio_file, dl_filename)
-            subprocess.check_output(convert_command, shell=True)
-            os.remove(raw_audio_file)
-            
-            print("Tagging...")
-            mp3_file = eyed3.load(dl_filename)
-            mp3_file.initTag()
-            mp3_file.tag.track_num = song_id3_tags["track_num"]
-            mp3_file.tag.title = song_id3_tags["title"]
-            mp3_file.tag.artist = album_id3_tags["artist"]
-            mp3_file.tag.album = album_id3_tags["album_name"]
-            mp3_file.tag.genre = album_id3_tags["genre"]
-            mp3_file.tag.year = album_id3_tags["year"]
-            mp3_file.tag.save()
-            print("")
+            if config["enable_multithreading"] == True:
+                new_dl_thread = threading.Thread(target=download_thread, args=(video_url, dl_filename, album_id3_tags, song_id3_tags))
+                dl_threads.append(new_dl_thread)
+            else:
+                download_thread(video_url, dl_filename, album_id3_tags, song_id3_tags)
             
             track_num = track_num + 1
+        
+        if config["enable_multithreading"] == True:
+            for dl_thread in dl_threads:
+                dl_thread.start()
+                
+            for dl_thread in dl_threads:
+                dl_thread.join()
             
         print("Download complete!")
         print("Downloaded album: {}".format(album_dir))
